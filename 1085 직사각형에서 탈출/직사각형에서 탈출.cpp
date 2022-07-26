0880f78c7e2c093aff2f1202c3e5f2ade98a9284
Bok Muchang <85085804+bokob@users.noreply.github.com>
2022-07-26 11:38:22 +0900

    Create 직사각형에서 탈출.cpp

---
+++ 1085 직사각형에서 탈출/직사각형에서 탈출.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+
+	vector<int> v;
+	v.push_back(x);
+	v.push_back(y);
+	v.push_back(w - x);
+	v.push_back(h - y);
+
+	cout << *min_element(v.begin(), v.end());
+
+	return 0;
+}
