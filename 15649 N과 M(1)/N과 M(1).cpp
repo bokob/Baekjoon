780af1257915bc68b5b94851850c6feee2d60944
Bok Muchang <85085804+bokob@users.noreply.github.com>
2022-07-27 18:14:40 +0900

    Create N과 M(1).cpp

---
+++ 15649 N과 M(1)/N과 M(1).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> v;
+	for (int i = 1; i <= N; i++)
+		v.push_back(i);
+
+	do
+	{
+		for (int i = 0; i < M; i++)
+			cout << v[i] << " ";
+		cout << "\n";
+
+		reverse(v.begin() + M, v.end());
+	} while (next_permutation(v.begin(), v.end()));
+}
