712bbe25ec6c32713386fe74f45e9e831071f17e
Bok Muchang <85085804+bokob@users.noreply.github.com>
2022-08-21 18:30:04 +0900

    Create 홍익대학교.cpp

---
+++ 16394 홍익대학교/홍익대학교.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	cout << N - 1946;
+
+	return 0;
+}
