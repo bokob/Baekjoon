98729802bf8c795481cf87eb2f4b6bcfc82a8228
Bok Muchang <85085804+bokob@users.noreply.github.com>
2022-09-04 14:48:44 +0900

    Create 알고리즘 수업 - 너비 우선 탐색 2.cpp

---
+++ 24445 알고리즘 수업 - 너비 우선 탐색 2/알고리즘 수업 - 너비 우선 탐색 2.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+bool cmp(int a, int b)
+{
+	return a > b;
+}
+
+vector<int> bfs(int start, vector<int> graph[], vector<bool>& visit)
+{
+	vector<int> v;
+	vector<int> r;
+	queue<int> q;
+	q.push(start);
+	visit[start] = true;
+
+	while (!q.empty())
+	{
+		int from = q.front();
+		q.pop();
+		v.push_back(from);
+
+		sort(graph[from].begin(), graph[from].end(), cmp);
+
+		for (long unsigned int i = 0; i < graph[from].size(); i++)
+		{
+			int to = graph[from][i];
+			if (visit[to] == false)
+			{
+				q.push(to);
+				visit[to] = true;
+			}
+		}
+	}
+	return v;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+
+	vector<int> v;
+	vector<int> r(N,0);
+
+	vector<int>* graph = new vector<int>[N + 1];
+	vector<bool> visit(N + 1, false);
+
+	for (int i = 0; i < M; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	v = bfs(R, graph, visit);
+
+	for (unsigned long int i = 0; i < v.size(); i++)
+		r[v[i] - 1] = i + 1;
+
+	for (int i = 0; i < N; i++)
+		cout << r[i] << "\n";
+
+	return 0;
+}
