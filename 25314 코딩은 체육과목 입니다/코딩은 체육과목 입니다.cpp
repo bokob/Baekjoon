da1e12c0f7f50a16725da8e598570b63d29c8402
Bok Muchang <85085804+bokob@users.noreply.github.com>
2022-09-17 23:34:00 +0900

    Create 코딩은 체육과목 입니다.cpp

---
+++ 25314 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, O;
+	cin >> N;
+	O = N / 4;
+
+	for (int i = 0; i < O; i++)
+		cout << "long ";
+	cout << "int";
+
+	return 0;
+}
