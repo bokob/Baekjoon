abfca6a0134974c4cb8a12605c2371b0e41e2dd9
Bok Muchang <85085804+bokob@users.noreply.github.com>
2022-09-21 23:03:59 +0900

    Update Hashing.cpp

--- 15829 Hasing/Hashing.cpp
+++ 15829 Hasing/Hashing.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int L;
+    cin >> L;
+    cin.ignore();
+
+    string s;
+    getline(cin, s);
+
+    long long H = 0, r = 1, M = 1234567891;
+    for (int i = 0; i < L; i++) 
+    {
+        H = (H + (s[i] - 'a' + 1) * r) % M;
+        r = r * 31 % M;
+    }
+    cout << H;
+    return 0;
+}
